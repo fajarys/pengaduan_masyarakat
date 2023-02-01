a628727ad56ba6e88406e1ed7f446994c7be000e
fajarys <fajargicung98@gmail.com>
2023-02-01 22:34:33 +0700

    chage color

--- resources/views/User/landing.blade.php
+++ resources/views/User/landing.blade.php
@@ -88,7 +88,7 @@
 </div>
 {{-- Section Hitung Pengaduan --}}
 <div class="pengaduan mt-5">
-    <div class="bg-purple">
+    <div class="bg-purple" >
         <div class="text-center">
             <h5 class="medium text-white mt-3">JUMLAH LAPORAN SEKARANG</h5>
             <h2 class="medium text-white">10</h2>
