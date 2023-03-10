44dc2c66ca4c1d9f77172f6bc335c11be96d49c2
fajarys <fajargicung98@gmail.com>
2023-03-10 08:38:12 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,12 +14,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Petugas::create([
-            'nama_petugas' => 'Adminstrator',
-            'username' => 'admins',
-            'password' => Hash::make('admin123'),
-            'telp' => '0877656543',
-            'level' => 'admin',
-        ]);
+        \App\Models\Petugas::create(
+            [
+                'nama_petugas' => 'Adminstrator',
+                'username' => 'admins',
+                'password' => Hash::make('admin123'),
+                'telp' => '0877656543',
+                'level' => 'admin',
+            ],
+            [
+                'nama_petugas' => 'Petugas',
+                'username' => 'petugas',
+                'password' => Hash::make('petugas123'),
+                'telp' => '08765434524',
+                'level' => 'petugas',
+            ]
+        );
     }
 }
