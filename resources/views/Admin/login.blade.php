811cb18a17fa0c6e024213c9ef449e47b4eb2c21
fajarys <fajargicung98@gmail.com>
2023-03-10 08:33:07 +0700

    title

--- resources/views/Admin/login.blade.php
+++ resources/views/Admin/login.blade.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
   integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-  <title>Login Petugas</title>
+  <title>Pengaduan Masyarakat</title>
 
   <link rel="website icon" type="png" href="{{ asset('images/laporan.png') }}">
   
